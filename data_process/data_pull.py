cf0a397a07786fabc809ea1234a3a1eb0887469e
Krystyna Furgacz <furgacz.krystyna@gmail.com>
2024-08-06 16:30:18 +0200

    Updated data pull file and pulled data from url

---
+++ data_process/data_pull.py
@@ -0,0 +1,17 @@
+# Importing required libraries
+import pandas as pd
+import requests
+from sklearn.model_selection import train_test_split
+import os
+
+url = 'https://en.wikipedia.org/wiki/Iris_flower_data_set'
+
+html = requests.get(url).content
+df_list = pd.read_html(html)
+data = pd.DataFrame(df_list[0])
+train, test, ytrain, ytest = train_test_split(data.iloc[:,:-1], data.Species, test_size = 0.2, shuffle = True)
+
+train = train.to_csv('./data_process/train.csv', index = False)
+ytrain = ytrain.to_csv('./data_process/ytrain.csv', index = False)
+test = test.to_csv('./data_process/test.csv', index = False)
+ytest = ytest.to_csv('./data_process/yest.csv', index = False)
